3f5df2cfabf47ba39535c197873ce8285b2cb259
Gil Watts <giljwatts@gmail.com>
2025-03-31 17:31:22 -0700

    inital commit

--- eventListener.js
+++
@@ -1,9 +0,0 @@
-//add event listener to call diceRollAndUpdate on enter
-document
-  .getElementById("numSides")
-  .addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      document.getElementById("roll-button").click();
-    }
-  });
